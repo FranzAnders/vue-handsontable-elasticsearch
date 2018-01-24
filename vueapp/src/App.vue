1f13157d76eda3381bdb75dc97343a805a00abcb
Rising Star <franz0420@hotmail.com>
2018-01-24 06:37:16 -0500

    Integrate handsontable component to vue app
    Signed-off-by: Rising Star <franz0420@hotmail.com>

--- vueapp/src/App.vue
+++ vueapp/src/App.vue
@@ -18,6 +18,9 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+#app > img {
+  height: 20vh;
+  margin: 2vh;
 }
 </style>
--- vueapp/src/components/HelloWorld.vue
+++ vueapp/src/components/HelloWorld.vue
@@ -1,113 +1,89 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+  <div id="example-container" class="wrapper">
+    <div id="hot-preview">
+      <HotTable :root="root" :settings="hotSettings"></HotTable>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+/* eslint-disable */
+  import HotTable from './vue-handsontable-official';
+  import Vue from 'vue';
+
+  export default {
+    data: function() {
+      return {
+        root: 'test-hot',
+        hotSettings: {
+          data: function(rows, columns) {
+            const result = [];
+            for (let i = 0; i < rows; i++) {
+              let row = [];
+              for (let j = 0; j < columns; j++) {
+                row.push('[' + i + ', ' + j + ']');
+              }
+              result.push(row);
+            }
+            return result;
+          }(1000, 7),
+          colHeaders: true,
+          rowHeaders: true,
+          columnSorting: true,
+          manualColumnResize: true
+        }
+      };
+    },
+    name: 'HelloWorld',
+    components: {
+      HotTable
     }
   }
-}
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style>
+  #example-container {
+    position: relative;
+  }
+
+  #hot-options {
+    width: 200px;
+    position: absolute;
+    top: 0;
+    left: 0;
+    padding: 15px;
+    font-size: 14px;
+  }
+
+  #hot-preview {
+  }
+
+  #test-hot {
+    max-width: 80vw;
+    height: 70vh;
+    margin: auto;
+    overflow: hidden;
+    width: fit-content;
+  }
+
+  .ht_master.handsontable {
+    width: fit-content;
+    margin: auto;
+  }
+
+  .ht_master .wtHolder {
+    width: fit-content !important;
+  }
+
+  table th, table td {
+    min-width: 100px;
+  }
+
+  .handsontable .columnSorting {
+    position: relative;
+    display: inline-block;
+    width: 100%;
+    height: 100%;
+    cursor: pointer;
+  }
 </style>
