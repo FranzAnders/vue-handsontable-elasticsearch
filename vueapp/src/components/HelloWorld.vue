c3c244ca38a38a9626c52f71c8104b46c9fd5267
Rising Star <franz0420@hotmail.com>
2018-01-24 16:49:48 -0500

    integrate api
    Signed-off-by: Rising Star <franz0420@hotmail.com>

--- vueapp/src/components/HelloWorld.vue
+++ vueapp/src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="example-container" class="wrapper">
-    <div id="hot-preview">
-      <HotTable :root="root" :settings="hotSettings"></HotTable>
+    <div id="hot-preview" v-if="rendering === true" >
+      <HotTable ref="hottable" :root="root" :settings="hotSettings" ></HotTable>
     </div>
   </div>
 </template>
@@ -10,29 +10,61 @@
 /* eslint-disable */
   import HotTable from './vue-handsontable-official';
   import Vue from 'vue';
+  import axios from 'axios'
+  import VueAxios from 'vue-axios'
+  import { HomeUrl } from './config'
+
+  Vue.use(VueAxios, axios)
 
   export default {
     data: function() {
       return {
+        rendering: false,
         root: 'test-hot',
+        hotTableKey: 'helloworld',
         hotSettings: {
-          data: function(rows, columns) {
-            const result = [];
-            for (let i = 0; i < rows; i++) {
-              let row = [];
-              for (let j = 0; j < columns; j++) {
-                row.push('[' + i + ', ' + j + ']');
-              }
-              result.push(row);
-            }
-            return result;
-          }(1000, 7),
+          data: undefined,
           colHeaders: true,
           rowHeaders: true,
           columnSorting: true,
           manualColumnResize: true
         }
       };
+    },
+    async created() {
+      this.rendering = false;
+      await this.getDataFromAPI()
+      
+    },
+    mounted: function() {
+      setTimeout(this.rendering = true, 4000);
+    },
+    methods: {
+      getHandsontableData() {
+        const data = this.$refs.hottable.table.getData()
+      },
+      getDataFromAPI() {
+        const api = `${HomeUrl}/web/_search`
+        console.log('api', api)
+        axios.get(api)
+        .then(response => {
+          const obj = response.data.hits.hits
+          const data = []
+      
+          obj.forEach(element => {
+            data.push([element._id,element._source.alt_names,
+            element._source.name,
+            element._source.latitude,
+            element._source.longitude,element._source.country_code,element._source.population
+            ])
+          });
+          console.log(data)
+          this.hotSettings.data = data;
+        })
+        .catch(e => {
+          this.errors.push(e)
+        })
+      }
     },
     name: 'HelloWorld',
     components: {
@@ -62,7 +94,7 @@
     max-width: 80vw;
     height: 70vh;
     margin: auto;
-    overflow: hidden;
+    overflow: scroll;
     width: fit-content;
   }
 
@@ -77,6 +109,9 @@
 
   table th, table td {
     min-width: 100px;
+    max-width: 300px;
+    max-height:300px;
+    overflow-y: scroll;
   }
 
   .handsontable .columnSorting {
---
+++ vueapp/src/components/config.js
@@ -0,0 +1,1 @@
+export const HomeUrl = 'http://13.211.42.88:9200'
